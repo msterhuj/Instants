f4a854b0a26b0067a4b118f900a91e3b055b26f2
msterhuj <gabin.lanore@gmail.com>
2021-05-03 19:37:23 +0200

    Added style to login page

--- src/App/Views/auth/login.php
+++ src/App/Views/auth/login.php
@@ -1,11 +1,51 @@
-<form method="post" action="/login">
-    <label for="username">Username</label>
-    <input id="username" name="username" type="text">
+<link rel="stylesheet" href="/css/auth.css">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="text-center login-box">
+            <div class="card bg-light shadow border-0">
+                <div class="card-body">
+                    <h2>Instants</h2>
+                    <h3>All now !</h3>
+                    <hr class="my-4">
+                    <h4>Please sign in</h4>
 
-    <label for="pass">Password</label>
-    <input id="pass" name="pass" type="password">
+                    <div class="text-center text-muted mb-4">
+                        <div class="alert alert-danger">
+                            <p>add error here if is set</p>
+                        </div>
+                    </div>
 
-    <input name="csrf" value="{{ CSRF }}" hidden>
+                    <form method="post" action="/login">
 
-    <button type="submit">login !</button>
-</form>
+                        <div class="form-group">
+                            <label for="username" class="control-label">Username</label>
+                            <div class="form-group">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    </div>
+                                    <input class="form-control" id="username" name="username" type="text">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="pass" class="control-label">Password</label>
+                            <div class="form-group">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                                    </div>
+                                    <input class="form-control" id="pass" name="pass" type="password">
+                                </div>
+                            </div>
+                        </div>
+
+                        <input name="csrf" value="{{ CSRF }}" hidden>
+                        <button class="btn btn-primary" type="submit">Login</button>
+                    </form>
+                    <p class="lead">By Instants.dev Teams</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- src/App/Views/templates/base.php
+++ src/App/Views/templates/base.php
@@ -9,12 +9,15 @@
         else
             echo '<link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">';
         ?>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 
     </head>
     <body>
-    {{ SYSTEM_CONTENT }}
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/js/theme.js"></script>
+
+        {{ SYSTEM_CONTENT }}
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="/assets/js/theme.js"></script>
     </body>
 </html>
