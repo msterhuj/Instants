ca49b5d2e867490ec63bff7218fd48d463b8952e
msterhuj <gabin.lanore@gmail.com>
2021-05-19 22:12:36 +0200

    Added base of card

---
+++ src/App/Views/home.php
@@ -0,0 +1,21 @@
+<div class="card">
+    <div class="card-header">
+        <!-- todo url profile here -->
+        <a href="#" class="d-block link-dark">
+            <img src="https://trello-members.s3.amazonaws.com/597720bcd3e8f7a90f1081a4/90eed9b54844deb9e5fdb4911c5fb3c1/30.png" alt="mdo" width="32" height="32" class="rounded-circle">
+            user name
+        </a>
+
+    </div>
+    <div class="card-body">
+        <p class="card-text">this is the nody content</p>
+    </div>
+    <div class="card-footer">
+        <a><i class="fas fa-heart"></i></a>
+        <a><i class="far fa-heart"></i></a>
+
+        <div class="card">
+            <div class="card-body">just test here</div>
+        </div>
+    </div>
+</div>
