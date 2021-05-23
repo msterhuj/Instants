2cbd58d4092218abab67f9e0f42766c4696693ff
msterhuj <gabin.lanore@gmail.com>
2021-05-23 19:20:12 +0200

    Added sec for api

--- src/App/Controller/ApiController.php
+++ src/App/Controller/ApiController.php
@@ -10,6 +10,7 @@ use Core\Router\Route;
 class ApiController extends Controller {
 
     public function post() {
+        if (Controller::isGuest()) return;
         if ($this->isPost()) {
             $data = $this->getBody();
             if (!empty($data['content'])) {
@@ -22,21 +23,25 @@ class ApiController extends Controller {
     }
 
     public function isLike() {
+        if (Controller::isGuest()) return;
         $post = Post::loadBy("id", Route::getRouteParam());
         echo $post->isLiked();
     }
 
     public function like() {
+        if (Controller::isGuest()) return;
         $post = Post::loadBy("id", Route::getRouteParam());
         $post->like();
     }
 
     public function isFollowee() {
+        if (Controller::isGuest()) return;
         $user = User::loadBy("id", Route::getRouteParam());
         echo $user->isFollowee();
     }
 
     public function followee() {
+        if (Controller::isGuest()) return;
         $user = User::getFromSession();
         $user->follow();
     }
