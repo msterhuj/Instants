4b3c72b1e582669d3a728afed8dc7f47697a8a15
msterhuj <gabin.lanore@gmail.com>
2021-05-31 16:45:09 +0200

    Base adm

---
+++ src/App/Views/admin/home.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="row info">
+        <div class="col-xs-6 col-md-3">
+            <div class="card">
+                <div class="card-header">Nombre de requeste aujouduit</div>
+                <div class="card-body">{{ REQ_DAY }}</div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-md-3"></div>
+        <div class="col-xs-6 col-md-3"></div>
+        <div class="col-xs-6 col-md-3"></div>
+    </div>
+    <div class="row graf">
+        <div class="col-sm-4"></div>
+        <div class="col-sm-4"></div>
+        <div class="col-sm-4"></div>
+    </div>
+    <div class="row log">
+        <div class="col-sm-12"></div>
+    </div>
+</div>
